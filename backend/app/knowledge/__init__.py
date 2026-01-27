52e0be220b0ef4fec980e677b23886f8ac65b341
suriyel <yue_shangmoon@126.com>
2026-01-27 22:25:59 +0800

    删除存量实现

--- backend/app/knowledge/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-知识库模块 - RAG 检索
-"""
-
-from .retriever import KnowledgeRetriever
-
-__all__ = ["KnowledgeRetriever"]
--- backend/app/tools/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Tool 定义模块
-"""
-
-from .base import get_default_tools, ToolRegistry
-
-__all__ = ["get_default_tools", "ToolRegistry"]
